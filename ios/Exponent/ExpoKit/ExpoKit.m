80b42853f905ef674af70a0ab42c6efb762f390e
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-05-14 12:06:52 +0200

    [expo-notifications] Apply requested changes

--- ios/Exponent/ExpoKit/ExpoKit.m
+++ ios/Exponent/ExpoKit/ExpoKit.m
@@ -80,7 +80,7 @@ NSString * const EXAppDidRegisterUserNotificationSettingsNotification = @"kEXApp
   return controller;
 }
 
-- (void)setUpApplication:(UIApplication *)application withLaunchOptions:(nullable NSDictionary *)launchOptions
+- (void)prepareWithLaunchOptions:(nullable NSDictionary *)launchOptions
 {
   [DDLog addLogger:[DDOSLogger sharedInstance]];
   RCTSetFatalHandler(handleFatalReactError);
